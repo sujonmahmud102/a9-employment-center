642f1503df4ee90edc28696e500ce7684d178a8d
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-04-09 22:00:39 +0600

    header components created

---
+++ src/components/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            applied jobs page
+        </div>
+    );
+};
+
+export default AppliedJobs;
---
+++ src/components/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            it is a blog page
+        </div>
+    );
+};
+
+export default Blog;
