2397971064b4b86ddf3cc164467c7b7a6634d849
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-04-12 10:55:35 +0600

    all card for company info added

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,21 +1,30 @@
 import React from 'react';
+import { MapPinIcon, CurrencyDollarIcon } from '@heroicons/react/24/solid'
+
 
 const Company = ({ company }) => {
     const { id, job_title, company_name, logo, location, part_time_or_full_time, remote_or_onsite,
         salary } = company
     return (
-        <div className='bg-[#eeebf5] mb-12 w-64 rounded-lg p-8'>
+        <div className='bg-[#eeebf5] mb-12 rounded-lg p-8 '>
 
-            <img src="" alt="" />
-            <h3 className='text-xl text-[#474747] font-semibold mb-2'>{job_title}</h3>
-            <p>{company_name}</p>
-            <h6>{remote_or_onsite}</h6>
-            <p>{location}</p>
-            <p>{salary}</p>
-            <button
-                className='text-white font-semibold px-4 py-3 rounded-md  bg-gradient-to-r from-[#7E90FE] to-[#9873FF] hover:from-pink-500 hover:to-yellow-500'>
-                View Details
-            </button>
+            <img className='w-24 h-12 mb-8' src={logo} alt="" />
+            <div>
+                <h3 className='text-xl text-[#474747] font-semibold mb-2'>{job_title}</h3>
+                <p className='text-[#757575] font-normal'>{company_name}</p>
+                <h6 className='text-transparent bg-clip-text bg-gradient-to-r from-[#7E90FE] to-[#9873FF] flex gap-3 mt-4'>
+                    <span className='border-2 border-[#7E90FE] px-3 py-1 rounded-md text-blue-600'>{remote_or_onsite}</span>
+                    <span className='border-2 border-[#7E90FE] px-3 py-1 rounded-md text-blue-600'>Full Time</span>
+                </h6>
+                <div className='flex gap-4 text-sm mt-4'>
+                    <p className='flex items-center gap-2'><MapPinIcon className="w-4 text-[#757575]" /> {location}</p>
+                    <p className='flex items-center gap-2'><CurrencyDollarIcon className="w-4 text-[#757575]" /> {salary}</p>
+                </div>
+                <button
+                    className='mt-4 text-white font-medium px-3 py-2 rounded-md  bg-gradient-to-r from-[#7E90FE] to-[#9873FF] hover:from-pink-500 hover:to-yellow-500'>
+                    View Details
+                </button>
+            </div>
 
         </div>
     );
