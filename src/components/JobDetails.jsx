ecd9bafc055b662c7f593dde817dfddb95ed84df
sujonmahmud102 <sujonmahmud102@gmail.com>
2023-04-12 15:18:40 +0600

    job details section done

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { CurrencyDollarIcon, CalendarDaysIcon, PhoneIcon, EnvelopeIcon, MapPinIcon  } from '@heroicons/react/24/solid'
 
 const JobDetails = () => {
     let { id } = useParams()
@@ -16,47 +17,72 @@ const JobDetails = () => {
     }, [])
 
     return (
-        <div className='flex justify-between px-12 mt-10'>
+       <section>
+        <h1 className='text-xl font-bold text-center mt-5'>
+                Job Details
+                </h1>
+         <div className='flex justify-between px-12 mt-10'>
             <div>
-                <p>
-                    Job Description: {job_description
+                
+                <p className='mb-4'>
+                    <span className='font-semibold'>
+                        Job Description:</span> {job_description
                     }
                 </p>
-                <p>
-                    Job Responsibility:  {job_responsibilities}
+                <p className='mb-4'>
+                    <span className='font-semibold'>
+                        Job Responsibility:</span>  {job_responsibilities}
                 </p>
-                <p>
-                    Educational Requirements: {educational_requirements}
+                <p className='mb-4'>
+                    <span className='font-semibold'>
+                        Educational Requirements:</span> {educational_requirements}
                 </p>
-                <p>
-                    Experiences: {experiences}
+                <p className='mb-4'>
+                    <span className='font-semibold'>
+                        Experiences:</span> {experiences}
                 </p>
             </div>
-            <div>
-                <h3>Bob Details</h3>
-                <p>
-                    Salary: {salary}
+            <div className='bg-blue-100 p-4 rounded-md'>
+                <h3 className='font-semibold border-b-2 border-indigo-300 pb-2'>Job Details</h3>
+                <p className='my-2 flex items-center gap-2'>
+                <CurrencyDollarIcon className="w-5 text-[#7E90FE]" />
+                    <span className='font-medium'>
+                        Salary:</span> {salary}
                 </p>
-                <p>
-                    Job Title: {job_title}
+                <p className='mb-2 flex items-center gap-2'>
+                <CalendarDaysIcon className="w-5 text-[#7E90FE]"  />
+                    <span className='font-medium'>
+                        Job Title:
+                    </span> {job_title}
                 </p>
-                <h3>
+                <h3 className='font-semibold border-b-2 border-indigo-300 pb-2'>
                     Contact Information
                 </h3>
-                <p>
-                    Phone: {contact_information?.phone}
+                <p className='my-2 flex items-center gap-2'>
+                <PhoneIcon className="w-5 text-[#7E90FE]" />
+                <span className='font-medium'>
+                
+                        Phone:
+                    </span> {contact_information?.phone}
                 </p>
-                <p>
-                    Email: {contact_information?.email}
+                <p className='my-2 flex items-center gap-2'>
+                <EnvelopeIcon className="w-5 text-[#7E90FE]"  />
+                <span className='font-medium'>
+                        Email:
+                    </span> {contact_information?.email}
                 </p>
-                <p>
-                    Address: {location}
+                <p className='my-2 flex items-center gap-2'>
+                <MapPinIcon className="w-5 text-[#7E90FE]"  />
+                <span className='font-medium'>
+                        Address:
+                    </span> {location}
                 </p>
-                <button>
+                <button className='w-full text-white font-semibold px-4 py-3 rounded-md  bg-gradient-to-r from-[#7E90FE] to-[#9873FF] hover:from-pink-500 hover:to-yellow-500'>
                     Apply Now
                 </button>
             </div>
         </div>
+       </section>
     );
 };
 
